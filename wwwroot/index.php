9a3ca210573a9bced30cb7fa3d0291486110fa31
Marc Görtz <mail@marcgoertz.de>
2021-01-29 22:51:51 +0100

    Initial commit.

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace Hyperion;
+namespace Marcgoertz\Hyperion;
 
 require(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
 use ogp\Parser as OgpParser;
 use Mf2 as Mf2Parser;
+
+header('Content-Type: application/json; charset=utf-8');
 
 $get = filter_var_array($_GET, FILTER_SANITIZE_STRING);
 
@@ -16,7 +18,6 @@ if (isset($get['url'])) {
         $ogp = OgpParser::parse($html);
         $mf2 = Mf2Parser\parse($html);
 
-        header('Content-Type: application/json; charset=utf-8');
         if (count($ogp) > 0 || count($mf2['items']) > 0 || count($mf2['rels']) > 0 || count($mf2['rel-urls']) > 0) {
             http_response_code(200);
             print json_encode($metadata);
